766a626fefc24dd79d59b89bd7d95a8a430762bc
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-08-20 20:50:58 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno {
+	public void estuda(int nivel){
+		//Método
+		if(nivel > 5){
+			System.out.println("syso + ctrl + espaco");
+		}else{
+			System.out.println("syso + ctrl + espaco");
+		}
+		
+	}
+	public void imprimeParametro(){
+		System.out.println("Um parámetro...");
+	}
+}
