a7bd12391eb0f91d6603472e6e5eaf9448fa1d23
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-08-26 20:12:41 -0300

    Add files via upload

---
+++ UsaProfessor.java
@@ -0,0 +1,10 @@
+package projeto;
+
+public class UsaProfessor {
+
+	public static void main(String[] args) {
+		Professor fujioka = new Professor();
+		fujioka.disciplina = "Metodologia e Linguagem de Progamação 2";
+		fujioka.ensina();
+	}
+}
